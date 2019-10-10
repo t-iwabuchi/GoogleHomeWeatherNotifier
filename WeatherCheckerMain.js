c3cd7bd65f73294deb55cf06673ee3c322bdcdae
takameron <takameron.info@gmail.com>
2019-10-10 22:45:51 +0900

    Fix: Method call from static method

--- WeatherCheckerMain.js
+++ WeatherCheckerMain.js
@@ -18,7 +18,7 @@ module.exports = class WeatherCheckerMain {
           notice : -1,
         }
 
-        if (isExistFile(datafile)) {
+        if (this.isExistFile(datafile)) {
           const file = JSON.parse(fs.readFileSync(datafile, 'utf8'))
           status.isRain = file.isRain
           status.notice = file.notice
@@ -28,7 +28,7 @@ module.exports = class WeatherCheckerMain {
         const current_time = weathers.filter(function(item, index){
           if (item.Type == "observation") return true;
         })[0].Date
-        const begin_time = begin_rainfall_time(weathers)
+        const begin_time = this.begin_rainfall_time(weathers)
 
         // 降雨予報がなければ、通知フラグはリセット
         if (begin_time == null)
@@ -39,14 +39,14 @@ module.exports = class WeatherCheckerMain {
         {
           const diff = begin_time - current_time
           status.notice = diff
-          googleHome.tell(`降雨通知です。${diff}分後に、${get_amount(weathers, begin_time)}ミリの雨が降りそうです。`)
+          googleHome.tell(`降雨通知です。${diff}分後に、${this.get_amount(weathers, begin_time)}ミリの雨が降りそうです。`)
         }
 
         // 降雨30分前
         if (status.isRain == false && status.notice > 30 && begin_time == current_time + 30)
         {
           status.notice = 30
-          googleHome.tell(`降雨通知です。30分後に、${get_amount(weathers, begin_time)}ミリの雨が降りそうです。`)
+          googleHome.tell(`降雨通知です。30分後に、${this.get_amount(weathers, begin_time)}ミリの雨が降りそうです。`)
         }
 
         // 降雨10分前
@@ -81,7 +81,7 @@ module.exports = class WeatherCheckerMain {
   }
 
   // ファイルの存在確認
-  isExistFile(file) {
+  static isExistFile(file) {
     try {
       fs.statSync(file)
       return true
@@ -91,7 +91,7 @@ module.exports = class WeatherCheckerMain {
   }
 
   // 降雨量が0でなくなる時間
-  begin_rainfall_time(weathers) {
+  static begin_rainfall_time(weathers) {
     const rainfall = weathers.filter(function(item, index){
       if (item.Rainfall != 0.0) return true;
     })
@@ -101,7 +101,7 @@ module.exports = class WeatherCheckerMain {
   }
 
   // 降雨量が0でなくなる時間
-  get_amount(weathers, time) {
+  static get_amount(weathers, time) {
     const match = weathers.filter(function(item, index){
       if (item.Date == time) return true;
     })
